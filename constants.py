ba7691906a6443cba0e2b2e87fc0e4993056e36d
myunyui22 <>
2021-08-26 00:16:09 +0900

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,10 @@
+PROCESSOR_VERSION = "0.5.0"
+
+# Entities
+AREAS = "areas"
+CAMERAS = "cameras"
+
+# Metrics
+OCCUPANCY = "occupancy"
+SOCIAL_DISTANCING = "social-distancing"
+FACEMASK_USAGE = "facemask-usage"
